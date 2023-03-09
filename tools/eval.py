0e47f7187143f63cba2be0ce226e85664561b9c9
alexchung <yonganzhong@outlook.com>
2023-03-09 14:44:56 +0800

    [feat] change default output path

--- tools/eval.py
+++ tools/eval.py
@@ -355,7 +355,7 @@ def parse_opt():
     parser.add_argument('--save-hybrid', action='store_true', help='save label+prediction hybrid results to *.txt')
     parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
     parser.add_argument('--save-json', action='store_true', help='save a COCO-JSON results file')
-    parser.add_argument('--project', default=ROOT / 'output/eval', help='save to project/name')
+    parser.add_argument('--project', default=ROOT / 'runs/eval', help='save to project/name')
     parser.add_argument('--name', default='exp', help='save to project/name')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
