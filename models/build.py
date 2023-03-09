ea13436b6f815e647df63f3af4ddc2e2b061a0e2
alexchung <yonganzhong@outlook.com>
2023-03-09 15:14:42 +0800

    [feat] add TTA(Test Time Augmentation)

--- models/build.py
+++ models/build.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 from models.network_blocks import Conv, DWConv
 from models import YOLO
 from utils.general import weight_load
+from utils.torch_utils import scale_img
 
 
 __all__ = ['DetectorModel']
@@ -21,7 +22,7 @@ class BaseModel(nn.Module):
     # YOLOv5 base model
     def forward(self, x, augment=False, profile=False, visualize=False):
         if augment:
-            pass
+            return self._forward_augment(x)  # TTA augment inference
         return self._forward_once(x, profile=profile, visualize=visualize)  # single-scale inference, train
 
     def _forward_once(self, x, profile=False, visualize=False):
@@ -29,6 +30,48 @@ class BaseModel(nn.Module):
         out = self.model(x)
 
         return out
+
+    def _forward_augment(self, x):
+        img_size = x.shape[-2:]  # height, width
+        s = [1, 0.83, 0.67]  # scales
+        f = [None, 3, None]  # flips (2-ud, 3-lr)
+        y = []  # outputs
+        for si, fi in zip(s, f):
+            xi = scale_img(x.flip(fi) if fi else x, si, gs=int(self.stride.max()))
+            yi = self._forward_once(xi)[0]  # forward
+            # cv2.imwrite(f'img_{si}.jpg', 255 * xi[0].cpu().numpy().transpose((1, 2, 0))[:, :, ::-1])  # save
+            yi = self._descale_pred(yi, fi, si, img_size)
+            y.append(yi)
+        y = self._clip_augmented(y)  # clip augmented tails
+        return torch.cat(y, 1), None  # augmented inference, train
+
+    def _descale_pred(self, p, flips, scale, img_size):
+        # de-scale predictions following augmented inference (inverse operation)
+        if self.inplace:
+            p[..., :4] /= scale  # de-scale
+            if flips == 2:
+                p[..., 1] = img_size[0] - p[..., 1]  # de-flip ud
+            elif flips == 3:
+                p[..., 0] = img_size[1] - p[..., 0]  # de-flip lr
+        else:
+            x, y, wh = p[..., 0:1] / scale, p[..., 1:2] / scale, p[..., 2:4] / scale  # de-scale
+            if flips == 2:
+                y = img_size[0] - y  # de-flip ud
+            elif flips == 3:
+                x = img_size[1] - x  # de-flip lr
+            p = torch.cat((x, y, wh, p[..., 4:]), -1)
+        return p
+
+    def _clip_augmented(self, y):
+        # Clip YOLOv5 augmented inference tails
+        nl = self.model[-1].nl  # number of detection layers (P3-P5)
+        g = sum(4 ** x for x in range(nl))  # grid points
+        e = 1  # exclude layer count
+        i = (y[0].shape[1] // g) * sum(4 ** x for x in range(e))  # indices
+        y[0] = y[0][:, :-i]  # large
+        i = (y[-1].shape[1] // g) * sum(4 ** (nl - 1 - x) for x in range(e))  # indices
+        y[-1] = y[-1][:, i:]  # small
+        return y
 
     def fuse(self):  # fuse model Conv2d() + BatchNorm2d() layers
         for m in self.model.modules():
